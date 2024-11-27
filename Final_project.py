2f2669d6a2c0d63b703638817ff9665df02fa86c
Letko <jzl6746@psu.edu>
2024-11-27 11:43:31 -0500

    more urls with more time stamps

--- Final_project.py
+++ Final_project.py
@@ -20,8 +20,9 @@ import requests
 
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
 #api key: BBCT3KLZKHATW4RP
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=BBCT3KLZKHATW4RP'
-r = requests.get(url)
+Dailyurl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=BBCT3KLZKHATW4RP'
+Weeklyurl = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=BBCT3KLZKHATW4RP'
+r = requests.get(Weeklyurl)
 data = r.json()
 
 print(data)
