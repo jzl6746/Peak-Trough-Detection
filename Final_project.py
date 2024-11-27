3e376f7a6b77e9a8c5dc8a4b58b8cceba89fa52b
Letko <jzl6746@psu.edu>
2024-11-27 11:11:47 -0500

    imports and API Key

---
+++ Final_project.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 27 10:25:46 2024
+
+@author: Letko
+"""
+
+import numpy as np
+import tensorflow as tf
+import keras
+import matplotlib
+import PyQt5
+
+from numpy import loadtxt
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+import requests
+
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+#api key: BBCT3KLZKHATW4RP
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=BBCT3KLZKHATW4RP'
+r = requests.get(url)
+data = r.json()
+
+print(data)
+
+
